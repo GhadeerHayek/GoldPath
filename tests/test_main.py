90924d15b027c38aa077cc5449312d6f0fcbbca4
GhadeerAlhayek <ghadeerhayek2001@gmail.com>
2026-07-28 14:48:16 +0300

    ruff reformat

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,11 +6,11 @@ from app.main import app
 
 BASE_URL = "http://0.0.0.0:8000"
 
+
 class DemoTest(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.client = TestClient(self.app)
-
 
     def test_health(self):
         response = self.client.get(f"{BASE_URL}/health")
